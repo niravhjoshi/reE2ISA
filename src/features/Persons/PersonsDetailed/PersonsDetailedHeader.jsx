f0861ccdeb7bf6ba2c4a11fd5a5c6eada39a280a
nijoshi <niravhjoshi@outlook.com>
2019-12-09 12:38:45 +0530

    Update on Delete Person Method

--- src/features/Persons/PersonsDetailed/PersonsDetailedHeader.jsx
+++ src/features/Persons/PersonsDetailed/PersonsDetailedHeader.jsx
@@ -1,79 +1,70 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Segment, Image, Item, Header, Button } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import { format } from 'date-fns';
-import { deletePerson } from '../personsActions';
-import { connect } from 'react-redux';
-
-const actions = {
-
-    deletePerson
-}
-class PersonsDetailedHeader extends Component {
-
-    handleDeletePerson = personID => {
-        this.props.deletePerson(personID);
-    }
 
 
-    render() {
-        const { person } = this.props;
+const personImageStyle = {
+    filter: 'brightness(30%)'
 
-        const personImageStyle = {
-            filter: 'brightness(30%)'
+};
+const personImageTextStyle =
+{
+    position: 'absolute',
+    bottom: '5%',
+    left: '5%',
+    width: '100%',
+    height: 'auto',
+    color: 'white'
+};
 
-        };
-        const personImageTextStyle =
-        {
-            position: 'absolute',
-            bottom: '5%',
-            left: '5%',
-            width: '100%',
-            height: 'auto',
-            color: 'white'
-        };
+const PersonsDetailedHeader = ({
+    person,
+    deletePerson,
 
-        return (
-            <Segment.Group>
-                <Segment basic attached="top" style={{ padding: '0' }}>
-                    <Image src={person.ImageURL} size="large" style={personImageStyle} />
+}) => {
 
-                    <Segment basic style={personImageTextStyle}>
-                        <Item.Group>
-                            <Item>
-                                <Item.Content>
-                                    <Header
-                                        size="huge"
-                                        content={person.FullName}
-                                        style={{ color: 'white' }}
-                                    />
-                                    <p>BirthDate:  <strong>{person.BirthDate && format(person.BirthDate.toDate(), 'EEEE do LLLL')}</strong></p>
-                                    <p>
-                                        Sex: <strong>{person.Sex}</strong>
-                                    </p>
-                                </Item.Content>
-                            </Item>
-                        </Item.Group>
-                    </Segment>
+
+    return (
+        <Segment.Group>
+            <Segment basic attached="top" style={{ padding: '0' }}>
+                <Image src={person.ImageURL} size="large" style={personImageStyle} />
+
+                <Segment basic style={personImageTextStyle}>
+                    <Item.Group>
+                        <Item>
+                            <Item.Content>
+                                <Header
+                                    size="huge"
+                                    content={person.FullName}
+                                    style={{ color: 'white' }}
+                                />
+                                <p>BirthDate:  <strong>{person.BirthDate && format(person.BirthDate.toDate(), 'EEEE do LLLL')}</strong></p>
+                                <p>
+                                    Sex: <strong>{person.Sex}</strong>
+                                </p>
+                            </Item.Content>
+                        </Item>
+                    </Item.Group>
                 </Segment>
+            </Segment>
 
-                <Segment attached="bottom">
-                    {/* <Button color="red" onClick={() => this.handleDeletePerson(person.id)}>
+            <Segment attached="bottom">
+                {/* <Button color="red" onClick={() => this.handleDeletePerson(person.id)}>
                         Delete Person</Button> */}
 
-                    <Button color="red" onClick={() => this.handleDeletePerson(person.id)}>
-                        Delete Person</Button>
-
-
-                    <Button color="orange" floated="right" as={Link} to={`/managePerson/${person.id}`}>
-                        Manage Person
+                <Button color="red" onClick={() => deletePerson(person.id)}>
+                    Delete Person</Button>
+                <Button color="orange" floated="right" as={Link} to={`/managePerson/${person.id}`}>
+                    Manage Person
                 </Button>
-                </Segment>
-            </Segment.Group >
-        )
+            </Segment>
+        </Segment.Group >
+    );
 
-    }
-}
+};
+
 
 // export default connect(mapState, actions)(PersonsDetailedHeader);
-export default connect(null, actions)(PersonsDetailedHeader);
+
+export default PersonsDetailedHeader;
